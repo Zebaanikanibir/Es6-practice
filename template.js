8483e1b4d0bd516cbf3ced9093bd2582bdd57442
Zebaccanikanibir <zanikanibir@gmail.com>
2021-02-01 01:11:33 +0600

    first

---
+++ template.js
@@ -0,0 +1,5 @@
+const firstName = 'justin';
+const lastName = 'TimberLake';
+const fullName = firstName + ' ' + lastName;
+const fullName2 = `${firstName} ${lastName} is a good boy`;
+console.log(fullName2);
